a7a54a413ada9a375f143510affc062948b38017
Alexis <70085218+AlexisMtzGasca@users.noreply.github.com>
2020-09-24 18:43:27 +0000

    Update dmenu.sh

--- .config/wm/dmenu.sh
+++ .config/wm/dmenu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Import the colors
-. "${HOME}/.cache/wal/colors.sh"
-
-dmenu_run -fn 'SFMonoNerdFont-10' -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15"
+# . "${HOME}/.cache/wal/colors.sh"
+#dmenu_run -fn 'SFMonoNerdFont-10' -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15"
+rofi -show run -display-run ""
